7633a145403bccaf0e8d03d855bb6bd98a1149d1
MichelOliveiraNas <michel.oliveira999@hotmail.com>
2020-01-22 22:54:51 -0300

    Correções no backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://DevMichel:michel999@cluster0-e6zxv.mongodb.net/week10?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://username:password@cluster0-e6zxv.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+
+app.use(cors());
 
 //EXPRESS JSON DEVE VIR ANTES DA CHAMADA DAS ROTAS.
 app.use(express.json());
